2184238eb8f77727ef9655faafb571776d3156bb
MAITRE Nicolas <Nicolas.MAITRE@cpnv.ch>
2020-02-12 10:59:58 +0100

    partof router install

--- cpnvago/src/App.vue
+++ cpnvago/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<router-link to="/test">Test link!</router-link>-->
   </div>
 </template>
 
