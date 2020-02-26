e469030ab03b3d715b9f78f117b821f53dc43a27
MAITRE Nicolas <Nicolas.MAITRE@cpnv.ch>
2020-02-26 11:00:52 +0100

    added router

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="topMenu">
+      <router-link to="/hello_world">Hello!</router-link>
+       | 
+      <router-link to="/home">Home!</router-link>
+    </div>
+    <router-view>
+    </router-view>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import VueRouter from "vue-router";
+//components
+import HelloWorld from "./components/HelloWorld.vue";
+import Home from "./components/Home.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: "/home", component: Home },
+    { path: "/hello_world", component: HelloWorld },
+  ]
+});
+export default {
+  name: "App",
+  router
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home-view">
+    <h1>Super page d'accueil de trivago</h1>
+    <p></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  props: {
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
