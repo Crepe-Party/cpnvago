8b92f3cb51b070bce227b80a54fa6be84f840f4d
Florian Bergmann <flobergmann@aol.ch>
2020-03-18 09:19:20 +0100

    split data

--- data/header.js
+++ data/header.js
@@ -71,17 +71,3 @@ const data_header = {
         'ZAR': 'ZAR - Rand sud-africain'
     }
 }
-
-const data_home = {
-    url_logo: '../assets/imgs/logo.png',
-    text_list: [
-        "Trouvez l’hôtel idéal et comparez les prix de différents sites Web",
-        "Essayez de rechercher une ville, un hôtel ou même un lieu connu."
-    ],
-    room_list: {
-        'single_room': 'Chambre simple',
-        'double_room': 'Chambre double',
-        'family_room': 'Chambres familliales',
-        'multiple_room': 'Chambres multiples'
-    }
-}
