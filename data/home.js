6775d63206a29fc2ebbab8694c81e3fb23ed8042
Florian Bergmann <flobergmann@aol.ch>
2020-03-18 10:14:08 +0100

    format data home in vue js prop

--- data/home.js
+++ data/home.js
@@ -1,13 +1,13 @@
 module.exports = {
     url_logo: '../assets/imgs/logo.png',
     text_list: [
-        "Trouvez l’hôtel idéal et comparez les prix de différents sites Web",
-        "Essayez de rechercher une ville, un hôtel ou même un lieu connu."
+        {text: "Trouvez l’hôtel idéal et comparez les prix de différents sites Web"},
+        {text: "Essayez de rechercher une ville, un hôtel ou même un lieu connu."}
     ],
-    room_list: {
-        'single_room': 'Chambre simple',
-        'double_room': 'Chambre double',
-        'family_room': 'Chambres familliales',
-        'multiple_room': 'Chambres multiples'
-    }
+    room_list: [
+        {value: 'single_room', text: 'Chambre simple'},
+        {value: 'double_room', text: 'Chambre double'},
+        {value: 'family_room', text: 'Chambres familliales'},
+        {value: 'multiple_room', text: 'Chambres multiples'}
+    ]
 }
